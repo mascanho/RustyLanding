4d92b175be819cb5a16f0b9d8966b536fe832542
mascanho <mascanho@sapo.pt>
2024-09-28 12:25:01 +0100

    boxes border fixed

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,13 +17,10 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
+              className="block relative p-0.5 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 md:max-w-[24rem] rounded-md"
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none bg-n-8">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
