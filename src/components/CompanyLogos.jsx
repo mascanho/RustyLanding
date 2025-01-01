c493e4354b07de56a264627dd7f49f62659549ae
mascanho <mascanho@sapo.pt>
2025-01-01 11:47:48 +0000

    Update CompanyLogos.jsx

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -4,7 +4,7 @@ const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/50">
-        Improving businesses&apos; SEO and Marketing
+        Elevating Brands. Driving Results
       </h5>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
