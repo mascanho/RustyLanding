88a657499f745a3e8381673d762c66ba63900d11
mascanho <mascanho@sapo.pt>
2025-01-01 11:38:11 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,8 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Batteries included Marketing &amp; SEO toolkit, {""}
             <span className="inline-block relative">
-              RustySEO{" "}
+              RustySEO,
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -34,6 +33,7 @@ const Hero = () => {
                 alt="Curve"
               />
             </span>
+            {""} the Next-Gen SEO Toolkit you need
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             No more tab hell or dozens of clicks to get your SEO insights.
