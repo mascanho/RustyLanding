20878867ab1e09156c22a5d3a7f48b816ef07452
Marco Guerreiro <mascanho@sapo.pt>
2025-04-05 20:55:43 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,18 +10,25 @@ const Footer = () => {
           © {new Date().getFullYear()}. All rights reserved.
         </p>
 
-        <ul className="flex gap-5 flex-wrap">
+        <a
+          className="text-sm text-white/40"
+          href="https://www.github.com/mascanho"
+        >
+          Built by Mascanho
+        </a>
+
+        {/* <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
             <a
               key={item.id}
               href={item.url}
               target="_blank"
-              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6 "
             >
               <img src={item.iconUrl} width={16} height={16} alt={item.title} />
             </a>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </Section>
   );
