779215465c9c45c3a03ee8f83e746fe2086f35c1
Marco Guerreiro <mascanho@sapo.pt>
2026-01-16 21:02:05 +0000

    fix: modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Modules from "./components/Modules";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
@@ -16,6 +17,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Modules />
         <Benefits />
         <UseCases />
         <Collaboration />
